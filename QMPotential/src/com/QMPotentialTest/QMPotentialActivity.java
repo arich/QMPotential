d723f9a29259e2f5ba677956c59a3b555b1aabe1
Andrew Rich <andrew.joseph.rich@gmail.com>
2012-02-03 13:43:39 -0600

    First Commit
    Committing for the very first time.... like a viiirrrgin

---
+++ QMPotential/src/com/QMPotentialTest/QMPotentialActivity.java
@@ -0,0 +1,15 @@
+package com.QMPotentialTest;
+
+import com.CapstonePhys.QMPotentialTest.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class QMPotentialActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
