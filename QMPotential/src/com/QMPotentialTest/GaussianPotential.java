ba70c7c8be2f044379ed69a623eb44173a43b76a
Andrew Rich <andrew.joseph.rich@gmail.com>
2012-04-02 15:29:55 -0500

    Final copy (I think)

--- QMPotential/src/com/QMPotentialTest/GaussianPotential.java
+++ QMPotential/src/com/QMPotentialTest/GaussianPotential.java
@@ -3,7 +3,26 @@ package com.QMPotentialTest;
 import java.lang.Math; // Gives Math.exp, Math.sin, etc.
 import java.io.*;
 import org.apache.commons.math.complex.*;
+/**
+ * GaussianPotential
+ * @author AndrewRich
+ * 
+ *  This file is part of QMPotential.
 
+    QMPotential is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    QMPotential is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with QMPotential.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 public class GaussianPotential {
 	//fields:
 	private final static double DXI = 0.01; //step by which to integrate
